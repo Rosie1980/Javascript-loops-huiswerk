6a1aeddf352ff19062d3c939af71396184b24720
Rosie1980 <r.derkinderen@gmail.com>
2023-07-20 13:49:45 +0200

    huiswerk gemaakt

--- loops-and-arrays.js
+++ loops-and-arrays.js
@@ -6,14 +6,15 @@
 
 const names = ["Henk", "Piet", "Fred", "Joop"];
 
+for (let i = 0; i < names.length; i++) {
+    console.log(names[i] + "je");
+}
+
 // Verwachtte uitkomsten:
 // Vóór het script zie je de originele waardes:
 // console.log(names) geeft: ["Henk", "Piet", "Fred", "Joop"]
 // Na jouw script zie je de aangepaste waardes:
 // console.log(names) geeft: ["Henkje", "Pietje", "Fredje", "Joopje"]
-
-
-
 
 
 // ==========================================
@@ -24,14 +25,22 @@ const names = ["Henk", "Piet", "Fred", "Joop"];
 
 const numbers = [2, 4, 5, 29, 38];
 
+for (let i = 0; i < numbers.length; i++) {
+        if (numbers[i] % 2 === 0) {
+            numbers[i] = numbers[i] * 2;
+        }
+    if (numbers[i] % 2 !== 0) {
+        numbers[i] = numbers[i] * 3;
+        }
+    }
+console.log(numbers);
+
+
 // Verwachtte uitkomsten:
 // Vóór het script zie je de originele waardes:
 // console.log(numbers) geeft: [2, 4, 5, 29, 38];
 // Na jouw script zie je de aangepaste waardes:
 // console.log(numbers) geeft: [ 4, 8, 15, 87, 76 ];
-
-
-
 
 
 // ==========================================
@@ -57,5 +66,17 @@ const squares = [30, 2, 8, 24, 11];
 //   'Het volume van 11 is 1331'
 // ]
 
+for (let i = 0; i < squares.length; i++) {
+        console.log("Het volume van " + squares[i] + " is " + (squares[i] * squares[i] * squares[i]));
+    }
+
+
+for (let i = 0; i < squares.length; i++) {
+    const volume = squares[i] * squares[i] * squares[i];
+
+    squares[i] = "Het volume van " + squares[i] + " is " + volume;
+}
+
+console.log(squares);
 
 
--- loops.js
+++ loops.js
@@ -10,11 +10,10 @@
 // Tip: je kunt de .repeat() methode gebruiken om een karakter een n aantal keer te herhalen...
 // Dit heb je nog niet geleerd, maar bekijk hiervoor dit MDN-artikel eens: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/repeat
 // ==========================================
-
-
-
-
-
+const star = "*";
+for (let i = 0; i < 5; i++) {
+    console.log(star.repeat(i + 1));
+}
 // ==========================================
 // Opdracht 2. Maak een for-loop die 4 keer het woord "loop..." logt, en bij de laatste (vijfde) loop het woord "klaar!"
 // Verwachte uitkomsten:
@@ -24,10 +23,17 @@
 // loop...
 // klaar!
 // ==========================================
-
-
-
-
+const loop = ["loop", "loop", "loop", "loop", "klaar!"];
+for (let i = 0; i < 5; i++) {
+    console.log(loop [i]);
+}
+for (let i = 0; i < 5; i++) {
+    if (i === 4) {
+        console.log('klaar!');
+    } else {
+        console.log('loop...');
+    }
+}
 
 // ==========================================
 // Opdracht 3: maak een for-loop die van 0 tot 9 loopt en de getallen 0 tot 9 logt.
@@ -44,9 +50,14 @@
 // >> 8
 // >> 9
 // ==========================================
-
-
-
+for (let i = 0; i < 10; i++) {
+    if (i <= 2) {
+        console.log(i);}
+    if ( i >= 3 && i <= 5) {
+    console.log(">"+ i);}
+    if ( i >= 6 && i <= 9) {
+        console.log(">>"+ i);}
+}
 
 
 // ==========================================
@@ -87,5 +98,18 @@
 // etc.
 // ==========================================
 
-
+for (let i = 1; i < 101; i++) {
+    if(i % 5 === 0 && i % 3 === 0){
+        console.log("FizzBuzz");
+    }
+    else if(i % 3 === 0){
+        console.log("Fizz");
+    }
+    else if(i % 5 === 0){
+        console.log("Buzz");
+    }
+    else {
+        console.log(i);
+    }
+}
 
